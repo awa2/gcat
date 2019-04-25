7d9c5fb7507fef9c1c14aa45b98e65fec7234f5c
imaizumitomonori <imaizumitomonori@crest.ocn.ne.jp>
2019-04-25 22:05:28 +0900

    20190425

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -19,6 +20,8 @@ type ARRAY []interface{}
 
 func main() {
 	var GcatData JSON = JSON{
+		// "share":        GetWmicShare(),
+		"Systeminfo":   GetUsers(),
 		"MacAddresses": GetMacAddresses(),
 	}
 
@@ -32,6 +35,82 @@ func main() {
 		log.Fatal(err)
 	}
 	fpw.Write(bytes)
+}
+
+type User struct {
+	Id   string `csv:"client_id"`
+	Name string `csv:"client_name"`
+	Age  string `csv:"client_age"`
+}
+
+//どうやらCSVの中身に空があるとエラーになるっぽいです。
+
+// func GetWmicShare() JSON {
+// 	// users := []*User{}
+// 	// ここでCSV形式の文字列を受け取るコマンドを実行する。
+// 	// arg := "list full /format:csv"
+// 	cmd := exec.Command("wmic", "share", "list", "/format:csv")
+// 	out, err := cmd.Output()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	data, err := conv(string(out))
+// 	fmt.Println(string(data))
+// 	// 	var in = `client_id,client_name,client_age
+// 	// user01,"J, Smith", 21`
+
+// 	r := csv.NewReader(strings.NewReader(data))
+// 	records, err := r.ReadAll()
+// 	fmt.Println(string("test"))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	json := JSON{}
+
+// 	var headers []string
+// 	for i, record := range records {
+
+// 		if i == 0 {
+// 			headers = record
+// 		} else {
+// 			for index, header := range headers {
+// 				json[header] = record[index]
+// 			}
+// 		}
+// 	}
+// 	return json
+// }
+
+func GetUsers() JSON {
+	// users := []*User{}
+	// ここでCSV形式の文字列を受け取るコマンドを実行する。
+	cmd := exec.Command("systeminfo", "/fo", "csv")
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := conv(string(out))
+	// 	var in = `client_id,client_name,client_age
+	// user01,"J, Smith", 21`
+
+	r := csv.NewReader(strings.NewReader(data))
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	json := JSON{}
+	var headers []string
+	for i, record := range records {
+		if i == 0 {
+			headers = record
+		} else {
+			for index, header := range headers {
+				json[header] = record[index]
+			}
+		}
+	}
+	return json
 }
 
 func GetMacAddresses() []string {
