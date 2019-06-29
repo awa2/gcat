081da2b52ac13d31faa3d6c8deb092efa50258c4
monkeydtofu <superchibihiro@gmail.com>
2019-06-29 16:52:46 +0900

    executor.goコメントアウト削除

--- common/executor.go
+++ common/executor.go
@@ -7,9 +7,5 @@ import (
 
 func ExecuteWinCmd(cmd string) ([]byte,error){
 	out, err := exec.Command("cmd.exe", "/k", "chcp 65001 && "+cmd).Output()
-    if err != nil {
-        return out,err
-	}
-
 	return out,err
 }
