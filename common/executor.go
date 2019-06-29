0bcd6dc1fd53617c6e4e1501ec56ae79d5f61cfe
monkeydtofu <superchibihiro@gmail.com>
2019-06-29 16:57:26 +0900

    returnぶち込み方式に変更

--- common/executor.go
+++ common/executor.go
@@ -6,6 +6,5 @@ import (
 )
 
 func ExecuteWinCmd(cmd string) ([]byte,error){
-	out, err := exec.Command("cmd.exe", "/k", "chcp 65001 && "+cmd).Output()
-	return out,err
+	return exec.Command("cmd.exe", "/k", "chcp 65001 && "+cmd).Output()
 }
